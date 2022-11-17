0839cbd3ca049b071d599e0dc34720d65853013c
Danira98 <danirsorg@gmail.com>
2022-11-16 20:34:05 -0800

    updated

--- js/app.js
+++ js/app.js
@@ -62,7 +62,7 @@ function updateFilters() {
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
     Object.entries(filters).forEach(([key,value]) => {// use ,entries to retrieve two things, use values to retrieve just the value
-      filteredData =  filteredData.filter(row => row[key]=== value)
+     filteredData =  filteredData.filter(row => row[key]=== value)
     });
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
